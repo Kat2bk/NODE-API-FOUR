47a82cb9e6ab4a8bab07764aa5e85ad450d8a28a
unknown <Katherine2bk@gmail.com>
2021-06-13 16:26:15 -0700

    set up morgan and cors, set up server.get request for home... set up a process.env variable for PORT. api working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./server');
 
-const port = 8080 || process.env.PORT;
+const port = 7800 || process.env.PORT;
 
-server.listen(() => {
+server.listen(port, () => {
     console.log(`\n** server is listening at ${port}** \n`)
 })
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const server = express();
+const morgan = require('morgan');
 const cors = require('cors');
 server.use(express.json());
+
+// endpoints, middleware, routers
+server.use(morgan('dev'))
+
+server.get('/', (req, res) => {
+    res.json({message: 'Welcome to the API Home'});
+})
 
 module.exports = server;
