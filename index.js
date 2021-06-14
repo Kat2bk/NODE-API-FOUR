bbaea9b203ad9b0a0e377778c286c8b0c0d1b1cd
unknown <Katherine2bk@gmail.com>
2021-06-13 19:54:18 -0700

    added PORT env variable, trying to get heroku to work

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require('./server');
 
-const port = 7800 || process.env.PORT;
+const PORT = 7800 || process.env.PORT;
 // heroku uses whatever port is available, we need that
 // option for it
 
-server.listen(port, () => {
+server.listen(PORT, () => {
     console.log(`\n** server is listening at ${port}** \n`)
 })
