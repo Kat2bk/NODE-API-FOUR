ec7cf7e41719692804142b5722a53baf38348b56
unknown <Katherine2bk@gmail.com>
2021-06-13 19:18:22 -0700

    added notes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./server');
 
 const port = 7800 || process.env.PORT;
+// heroku uses whatever port is available, we need that
+// option for it
 
 server.listen(port, () => {
     console.log(`\n** server is listening at ${port}** \n`)
