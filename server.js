8f1635de237179b8f031b52733b48b2191500eb6
unknown <Katherine2bk@gmail.com>
2021-06-13 18:31:32 -0700

    set up a NODE_ENV for using cors.

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const morgan = require('morgan');
-const cors = require('cors');
 server.use(express.json());
 
 // endpoints, middleware, routers
 server.use(morgan('dev'))
+
+if (process.env.NODE_ENV === 'development') {
+    const cors = require('cors');
+    server.use(cors())
+}
+
+// on heroku, node_env will be production, but we do
+// not want to enable cors when developing an indepedent
+// self-contained program.
 
 server.get('/', (req, res) => {
     res.json({message: 'Welcome to the API Home'});
